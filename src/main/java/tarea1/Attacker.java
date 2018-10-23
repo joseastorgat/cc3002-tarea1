5fdc78e8fdd7558253e82b575ceb551dfe466030
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-10-23 01:51:39 -0300

    work in Attacker and Entity Interface

--- src/main/java/tarea1/Attacker.java
+++ src/main/java/tarea1/Attacker.java
@@ -9,5 +9,79 @@ import tarea1.Entity;
  */
 
 public interface Attacker extends Entity {
+
+	 /**
+     * method to obtain the AttackPoints of the object
+     *
+     * @return AttackPoints of the object
+     */
+ 
+    double getAttackPoints();
+
+	 /**
+     * method to attack an Entity object
+     *
+	 * @param entity Enitity to be attacked 
+     */
+    void attack(Entity entity);
+    /**
+	* method to attack a InfantryUnit
+	* 
+	* @param infantry InfantryUnit to be attacked
+    */
+    void attackInfantryUnit(InfantryUnit infantry);
+    
+
+    /**
+	* method to attack a CavalryUnit 
+	* 
+	* @param cavalry CavalryUnit to be attacked
+    */
+    void attackCavalryUnit(CavalryUnit cavalry);
+    
+    
+    /**
+	* method to attack a SiegeUnit
+	* 
+	* @param siege SiegeUnit to be attacked
+    */
+    void attackSiegeUnit(SiegeUnit siege);
+    
+    
+    /**
+	* method to attack a ArcherUnit
+	* 
+	* @param archer ArcherUnit to be attacked
+    */
+    void attackArcherUnit(ArcherUnit archer);
+    
+
+    /**
+	* method to attack a Villager
+	* 
+	* @param villager Villager to be attacked
+    */
+    void attackVillager(Villager villager);
+    
+    /**
+	* method to attack a Monk
+	* 
+	* @param monk Monk to be attacked
+    */
+    void attackMonk(Monk monk);
+    
+    /**
+	* method to attack a Castle
+	* 
+	* @param castle Castle to be attacked
+    */
+    void attackCastle(Castle castle);
+    
+    /**
+	* method to attack a Barrack
+	* 
+	* @param barrack Barrack to be attacked
+    */
+    void attackBarrack(Barrack barrack);
    
 }
--- src/main/java/tarea1/Entity.java
+++ src/main/java/tarea1/Entity.java
@@ -9,4 +9,54 @@ import tarea1.Attacker;
  * @author Jose Astorga
  */
 public interface Entity {
+
+    /**
+     * method to obtain the actual HitPoints of the object
+     *
+     * @return HitPoints of the object
+     */
+    
+    double getHp();
+
+    /**
+     * method to obtain the max Hitpoint of the object
+     *
+     * @return maxHitPoints of the object
+     */
+
+    double getMaxHp();
+
+
+    /**
+     * method to deal with a attack to the object
+     *
+     * @param damage Damage delivered to the object
+     */
+    void receiveDamage(double damage);
+
+
+    /**
+     * method to deal with an 
+     *
+     * @param health  Health delivered to the object
+     */    
+    void heal(double health);
+
+
+    /**
+     * method to check if the entity is live ( hp>0 )
+     
+     * @return  Boolean indicating if the entity isAlive 
+     */
+    boolean isAlive();
+
+
+    /**
+     * method to deal with an attack to the Object
+     *
+     * @param attacker Attacker attacking the Object
+     */
+
+    void attackedBy(Attacker attacker);
+
 }
