c89b721c2171659a54ef0974fb2b09c1950b9175
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-10-23 01:49:29 -0300

    add some testing stuff

---
+++ src/test/java/tarea1/BarracksTest.java
@@ -0,0 +1,30 @@
+import tarea1.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BarracksUnitTest {
+    
+    private Entity infantry;
+    private Entity cavalry;
+    private Entity archer;
+    private Entity siege;
+    private Entity monk;
+    private Entity villager;
+    private Entity barracks;
+    private Entity castle;
+
+    @Before
+    public void setUp(){
+        // infantry  = new InfantryUnit()
+        // cavalry   = new CavalryUnit()
+        // archer    = new ArcherUnit()
+        // siege     = new SiegeUnit()
+        // monk      = new Monk()
+        // villager  = new Villager()
+    }
+
+    @Test
+    public void cantKeepStanding(){
+    }
+}
---
+++ src/test/java/tarea1/CavalryUnitTest.java
@@ -0,0 +1,61 @@
+import tarea1.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CavalryUnitTest {
+    
+    private Entity infantry;
+    private Entity cavalry;
+    private Entity archer;
+    private Entity siege;
+    private Entity monk;
+    private Entity villager;
+    private Entity barracks;
+    private Entity castle;
+
+    @Before
+    public void setUp(){
+        // infantry  = new InfantryUnit()
+        // cavalry   = new CavalryUnit()
+        // archer    = new ArcherUnit()
+        // siege     = new SiegeUnit()
+        // monk      = new Monk()
+        // villager  = new Villager()
+    }
+    @Test
+    public void attackInfantryUnitTest(){
+    }
+
+    @Test
+    public void attackCavalryUnitTest(){
+    }
+
+    @Test
+    public void attackArcherUnitTest(){
+    }
+
+    @Test
+    public void attackSiegeUnitTest(){
+    }
+
+    @Test
+    public void attackMonkTest(){
+    }
+
+    @Test
+    public void attackVillagerTest(){
+    }
+
+   @Test
+    public void attackBarracksTest(){
+    }
+
+   @Test
+    public void attackCastleTest(){
+    }
+
+    @Test
+    public void cantKeepFighting(){
+    }
+}
