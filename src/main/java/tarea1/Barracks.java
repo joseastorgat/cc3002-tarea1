909c25d76a150bb94679de5a46f147dea95ef97a
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-10-25 06:15:56 -0300

    adding Buildings

--- src/main/java/tarea1/Barracks.java
+++ src/main/java/tarea1/Barracks.java
@@ -7,11 +7,43 @@ package tarea1;
  */
 
 public class Barracks extends AbstractAttackable{
+    private static final double BARRACK_HP=500;
+	public Barracks(){
+		super(BARRACK_HP, BARRACK_HP);
+	}
 
-
-	public void attackedBy(Attacker attacker){
-		attacker.attackBarracks(this);
+	@Override
+    public void attackedByArcher(ArcherUnit archer){
+    	this.receiveDamage(archer.getAttackPoints()*0.7);
     }
+	
+	@Override
+    public void attackedByCavalry(CavalryUnit cavalry){
+    	this.receiveDamage(cavalry.getAttackPoints()*0.7);
+    }
+	
+	@Override
+    public void attackedByCastle(Castle castle){
+    	this.receiveDamage(castle.getAttackPoints()*0.7);
+    }
+
+    @Override
+	public void attackedByInfantry(InfantryUnit infantry){
+    	this.receiveDamage(infantry.getAttackPoints()*0.7);
+	}
+        
+    @Override
+    public void attackedBySiegeUnit(SiegeUnit siege){
+    	this.receiveDamage(siege.getAttackPoints()*2.0);
+	}
+    
+    @Override
+    public void attackedByVillager(Villager villager){
+    	this.heal(villager.getAttackPoints()*0.7);
+	}
+
+	@Override
+	public void attackedByMonk(Monk monk){}
 
 
 }
--- src/main/java/tarea1/Castle.java
+++ src/main/java/tarea1/Castle.java
@@ -7,10 +7,47 @@ package tarea1;
  */
 
 public class Castle extends AbstractAttacker{
+    private static final double CASTLE_HP=1000;
+    private static final double CASTLE_DMG_FACTOR=20;
 
+	public Castle(){
+        super(CASTLE_HP, CASTLE_HP, CASTLE_DMG_FACTOR);
+	}
 
-	public void attackedBy(Attacker attacker){
-		attacker.attackCastle(this);
+	public void attack(Entity entity){
+		entity.attackedByCastle(this);
     }
+
+	@Override
+    public void attackedByArcher(ArcherUnit archer){
+    	this.receiveDamage(archer.getAttackPoints() *0.1);
+    }
+	
+	@Override
+    public void attackedByCavalry(CavalryUnit cavalry){
+    	this.receiveDamage(cavalry.getAttackPoints()* 0.3);
+    }
+	
+	@Override
+    public void attackedByCastle(Castle castle){
+    	this.receiveDamage(castle.getAttackPoints()*0.1);
+    }
+
+    @Override
+	public void attackedByInfantry(InfantryUnit infantry){
+    	this.receiveDamage(infantry.getAttackPoints()*0.3);
+	}
+        
+    @Override
+    public void attackedBySiegeUnit(SiegeUnit siege){
+    	this.receiveDamage(siege.getAttackPoints()*2.0);
+	}
+    
+    @Override
+    public void attackedByVillager(Villager villager){
+    	this.heal(villager.getAttackPoints()*0.3);
+	}	
+	@Override
+	public void attackedByMonk(Monk monk){}
 
 }
